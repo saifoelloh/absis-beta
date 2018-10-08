dfbd99187d0ad95d35ea104bfab1c40e23aff6b1
saifoelloh <saifoelloh@gmail.com>
2018-10-08 12:29:10 +0700

    update some method

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,4 @@
 const Joi = require('joi');
-const Axios = require('axios');
 
 const User = require('../models/user');
 
@@ -19,24 +18,6 @@ module.exports = {
             grade: req.body.grade,
             subdomain: req.body.subdomain,
         });
-        Axios.get('https://antareja.iixcp.rumahweb.com:2083/cpsess8854196392/json-api/cpanel', {
-            params:{
-                cpanel_jsonapi_user: 'absh5686',
-                cpanel_jsonapi_apiversion: '2',
-                cpanel_jsonapi_module: 'SubDomain',
-                cpanel_jsonapi_func: 'addsubdomain',
-                domain: user.subdomain,
-                rootdomain: 'absis-beta.com',
-                dir: '%2Fpublic_html%2Fdirectory_name',
-                disallowdot: 1,
-            }
-        })
-        .then(function(res) {
-            console.log(res);
-        })
-        .catch(function(err) {
-            return console.log(err);
-        })
         user.save(function(err) {
             if(err) return res.status(500).send(err);
             res.status('200').send('User saved');
